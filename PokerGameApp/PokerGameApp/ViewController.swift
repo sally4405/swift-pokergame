c1075c30695eff5084556ed4928c0e8cb28e037b
Lee SeEun <sally4405@naver.com>
2022-02-22 10:42:00 +0900

    [step1] feat: 배경 패턴 이미지 추가하고 적용

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -3,6 +3,10 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        if let backgroundImage = UIImage(named: "bg_pattern.png") {
+            self.view.backgroundColor = UIColor(patternImage: backgroundImage)
+        }
+        
         super.viewDidLoad()
     }
 
