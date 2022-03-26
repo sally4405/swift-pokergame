2d1edb9dc751f653e34ff6c40b733e87f4348872
Lee SeEun <sally4405@naver.com>
2022-03-26 16:18:48 +0900

    [step1] feat: 카드 7장 생성하여 배치

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -1,14 +1,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
+        super.viewDidLoad()
+        
         if let backgroundImage = UIImage(named: "bg_pattern.png") {
             self.view.backgroundColor = UIColor(patternImage: backgroundImage)
         }
         
-        super.viewDidLoad()
+        (0...6).forEach { setupView(leadingTag: CGFloat($0)) }
     }
+    
+    func setupView(leadingTag: CGFloat) {
+        let cardView = UIImageView()
+        cardView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(cardView)
 
+        let aspectRatio: CGFloat = 1.27 / 1
+        let widthRatio: CGFloat = 1 / 7
+        
+        cardView.image = UIImage(named: "card-back")
+
+        NSLayoutConstraint.activate([
+            cardView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            cardView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: leadingTag * view.frame.width * widthRatio),
+            cardView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: widthRatio),
+            cardView.heightAnchor.constraint(equalTo: cardView.widthAnchor, multiplier: aspectRatio)
+        ])
+    }
+    
 }
-
